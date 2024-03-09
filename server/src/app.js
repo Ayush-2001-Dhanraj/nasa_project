d0a8c1c3e0c6b3cb734b005005cd7669f228a28d
Ayush Dhanraj <dhanrajaayush123@gmail.com>
2024-03-09 16:07:30 +0530

    build | install | run - scripts

--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const cors = require("cors");
 
 const planetRouter = require("./routes/planets/planets.router");
@@ -11,6 +12,10 @@ app.use(
   })
 );
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
 app.use("/planets", planetRouter);
+app.get("/", (req, res) => {
+  return res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 
 module.exports = app;
