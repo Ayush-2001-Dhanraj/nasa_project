2a8c794263d82a8815a6d7135fcb97be67c9445c
Ayush Dhanraj <dhanrajaayush123@gmail.com>
2024-03-11 16:33:02 +0530

    Start mongoDB integration

--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,22 @@
 const http = require("http");
+const mongoose = require("mongoose");
 
 const app = require("./app");
 const { loadPlanetsData } = require("./models/planets.model");
 
 const PORT = process.env.PORT || 8000;
+const CONNECTION_URL =
+  "mongodb+srv://dev:sFbBKCILcR5TY3WQ@nasa-api.of0uvl7.mongodb.net/?retryWrites=true&w=majority&appName=nasa-api";
 
 const server = http.createServer(app);
 
+mongoose.connection.on("open", () => console.log("MongoDB connected"));
+
+mongoose.connection.on("error", (err) => console.error(err));
+
 const startServer = async () => {
+  await mongoose.connect(CONNECTION_URL);
+
   await loadPlanetsData();
   server.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
